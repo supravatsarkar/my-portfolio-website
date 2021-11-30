3dcb2913dc4be3d6e39e02c5a7f15cb82f9a129c
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-11-30 11:00:21 +0530

    add loading

--- src/Component/Home/Home/Home.js
+++ src/Component/Home/Home/Home.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Skills from '../Skills/Skills';
 import TopBanner from '../TopBanner/TopBanner';
+import { BoxesLoader } from "react-awesome-loaders";
 
 const Home = () => {
+    const [loading, setLoading] = useState(true);
+    window.addEventListener('load', (event) => {
+        setLoading(false)
+        console.log('page is fully loaded');
+    });
     return (
-        <div style={{ height: '2000px', backgroundColor: '#2B1EC2' }}>
-            <TopBanner></TopBanner>
-            <Skills></Skills>
+        <div >
+            {
+                loading ?
+                    <BoxesLoader className='mx-auto'
+                        boxColor={"#6366F1"}
+                        style={{ marginBottom: "20px" }}
+                        desktopSize={"128px"}
+                        mobileSize={"80px"}
+                    />
+                    :
+                    <div style={{ height: '2000px', backgroundColor: '#2B1EC2' }}>
+                        <TopBanner></TopBanner>
+                        <Skills></Skills>
+                    </div>
+            }
+
+
         </div>
     );
 };
